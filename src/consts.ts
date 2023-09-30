ded8da5be5debd4ab40837afc22a1a697819421f
Giulio Zanchetta <zank@outlook.it>
2023-09-30 17:47:31 +0200

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,46 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+// Website metadata
+export const SITE_URL: string = "https://example.com/";
+export const SITE_TITLE: string = "Astro Blog";
+export const SITE_DESCRIPTION: string = "Welcome to my website!";
+
+// Repo info
+export const REPO: string = "zankhq/astro-starter"; // TODO: Update dynamically
+export const DEFAULT_BRANCH: string = "main"; // TODO: Update dynamically
+
+// SEO metadata
+export const TWITTER_CREATOR: string = "@xxx";
+
+// Navigation
+type Page = {
+	title: string;
+	href: string;
+	children?: Page[];
+};
+
+export const PAGES: Page[] = [
+	{
+		title: "Home",
+		href: "/",
+	},
+	{
+		title: "Blog",
+		href: "/blog",
+	},
+	{
+		title: "About",
+		href: "/about",
+	},
+];
+
+// i18n
+export const DEFAULT_LOCALE = "en";
+export const LOCALES = {
+	en: "en", // the `defaultLocale` value must present in `locales` keys
+	it: "it",
+};
+
+// Hosting
+export const HOSTING_SERVICE: "cloudflare" | "netlify" | "none" = "cloudflare";
